fa4e81c73ecc93131d1720e4616c1e7c3c1590c7
b-gtr <aloaloalter@gmail.com>
2024-12-02 11:59:59 +0100

    Update dfs_with_wrapper.py

--- dfs_with_wrapper.py
+++ dfs_with_wrapper.py
@@ -11,32 +11,31 @@ import numpy as np
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Initialize the environment
-# Set the parameters for gym_carla
 env_params = {
-    'number_of_vehicles': 30,
-    'number_of_walkers': 10,
-    'display_size': 640,  # Screen size of bird-eye render
-    'max_past_step': 1,   # The number of past steps to draw
-    'dt': 0.1,  # Time interval between two frames
-    'discrete': False,  # Whether to use discrete control space
-    'discrete_acc': [1.0],  # Discrete value of accelerations
+    'number_of_vehicles': 0,  # Set to 0 as per your request
+    'number_of_walkers': 0,   # Set to 0 as per your request
+    'display_size': 480,      # Height of the display (adjusted to 480)
+    'max_past_step': 1,       # The number of past steps to draw
+    'dt': 0.1,                # Time interval between two frames
+    'discrete': False,        # Whether to use discrete control space
+    'discrete_acc': [1.0],    # Discrete value of accelerations
     'discrete_steer': [-0.2, 0.0, 0.2],  # Discrete value of steering angles
     'continuous_accel_range': [-1.0, 1.0],  # Continuous acceleration range
     'continuous_steer_range': [-0.3, 0.3],  # Continuous steering angle range
     'ego_vehicle_filter': 'vehicle.lincoln*',  # Filter for defining ego vehicle
-    'port': 2000,  # Connection port
-    'town': 'Town03',  # Which town to simulate
-    'task_mode': 'random',  # Mode of the task
+    'port': 2000,             # Connection port
+    'town': 'Town03',         # Which town to simulate
+    'task_mode': 'random',    # Mode of the task
     'max_time_episode': 500,  # Maximum timesteps per episode
-    'max_waypt': 12,  # Maximum number of waypoints
-    'obs_range': 32,  # Observation range (meter)
-    'lidar_bin': 0.125,  # Bin size of lidar sensor (meter)
-    'd_behind': 12,  # Distance behind the ego vehicle (meter)
-    'out_lane_thres': 2.0,  # Threshold for out of lane
-    'desired_speed': 8,  # Desired speed (m/s)
+    'max_waypt': 12,          # Maximum number of waypoints
+    'obs_range': 32,          # Observation range (meter)
+    'lidar_bin': 0.125,       # Bin size of lidar sensor (meter)
+    'd_behind': 12,           # Distance behind the ego vehicle (meter)
+    'out_lane_thres': 2.0,    # Threshold for out of lane
+    'desired_speed': 8,       # Desired speed (m/s)
     'max_ego_spawn_times': 200,  # Maximum times to spawn ego vehicle
-    'display_route': True,  # Whether to display the route
-    'pixor_size': 64,  # Size of the PIXOR image
+    'display_route': True,    # Whether to display the route
+    'pixor_size': 64,         # Size of the PIXOR image
 }
 
 env = gym.make('carla-v0', params=env_params)
